a3af114e1f5297bd95bc6bfa573eeff3d6a0e5c1
Dion Feng <55362194+DioFeng@users.noreply.github.com>
2021-07-30 00:23:29 -0700

    Update Roads.java

--- Roads.java
+++ Roads.java
@@ -1,77 +1,52 @@
-import java.io.File; 
-import java.io.FileNotFoundException; 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
-public class Roads
-{
-	private List<Integer> distance;
-	private List<Integer> time;
-	private List<String> initCity, endCity;
+public class Roads {
+    private final List<Integer> distance;
+    private final List<Integer> time;
+    private final List<String> initCity, endCity;
 
-	public Roads()
-	{
-		distance = new ArrayList<Integer>();
-		time = new ArrayList<Integer>();
-		initCity = new ArrayList<String>();
-		endCity = new ArrayList<String>();
-		importData(); // initiate lists
-	}
-	// store data
-	private void importData()
-	{
-		try
-		{	
-			File file = new File("roads.csv");
-			Scanner sc = new Scanner(file);
-			//sc.nextLine();
-			while(sc.hasNextLine())
-			{
-				String line = sc.nextLine();
-				String[] result = line.split(",");
-				
-				endCity.add(result[0]);
-				initCity.add(result[1]);
-				int d = Integer.parseInt(result[2]);
-				distance.add(d);
-				int t = Integer.parseInt(result[3]);
-				time.add(t);
-			}
+    public Roads()
+    {
+        distance = new ArrayList<>();
+        time = new ArrayList<>();
+        initCity = new ArrayList<>();
+        endCity = new ArrayList<>();
+        importData(); // initiate lists
+    }
+    // store data
+    private void importData() {
+        try {
+            File file = new File("src/roads.csv");
+            Scanner sc = new Scanner(file);
+            //sc.nextLine();
+            while(sc.hasNextLine())
+            {
+                String line = sc.nextLine();
+                String[] result = line.split(",");
 
-		}
-		catch (FileNotFoundException e)
-		{
-			System.out.println("Not such file");
-		}
-	}
-	public String getEndCity(int i)//get city
-	{
-		return endCity.get(i);
-	}
+                endCity.add(result[0]);
+                initCity.add(result[1]);
+                int d = Integer.parseInt(result[2]);
+                distance.add(d);
+                int t = Integer.parseInt(result[3]);
+                time.add(t);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Not such file");
+        }
+    }
+    public String getEndCity(int i) { return endCity.get(i); }
 
-	public String getInitCity(int i)//get city
-	{
-		return initCity.get(i);
-	}
-
-	public int getDistance(int i)//cost
-	{
-		return distance.get(i);
-	}
-
-	public int getMinute(int i)//cost
-	{
-		return time.get(i);
-	}
-	public int getSize()
-	{
-		return endCity.size();
-	}
-	public List<String> endCity()
-	{
-		return endCity;
-	}
-	public List<String> initCity()
-	{
-		return initCity;
-	}
+    public String getInitCity(int i) { return initCity.get(i); }
+    //cost
+    public int getDistance(int i) { return distance.get(i); }
+    //cost
+    public int getMinute(int i) { return time.get(i); }
+    public int getSize() { return endCity.size(); }
+    public List<String> endCity() { return endCity; }
+    public List<String> initCity() { return initCity; }
 }
