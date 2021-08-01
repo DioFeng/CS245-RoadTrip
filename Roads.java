c5077c4126baf56b4c3dfbc0ac9b80916d72c552
Dion Feng <55362194+DioFeng@users.noreply.github.com>
2021-07-31 20:42:51 -0700

    update file location

--- Roads.java
+++ Roads.java
@@ -18,7 +18,7 @@ public class Roads {
     // store data
     private void importData() {
         try {
-            File file = new File("src/roads.csv");
+            File file = new File("roads.csv");
             Scanner sc = new Scanner(file);
             //sc.nextLine();
             while(sc.hasNextLine())
