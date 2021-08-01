a01e6672015e0240512cc67f5c1e91e8e3c1443f
Dion Feng <55362194+DioFeng@users.noreply.github.com>
2021-07-31 20:17:50 -0700

    Added comments

--- City.java
+++ City.java
@@ -1,13 +1,42 @@
 import java.util.*;
 public class City {
     final static Roads r = new Roads();
-    HashSet<String> citiesSet = new HashSet<>();
-    ArrayList<String> combine; //<All cities no duplicate>
-    ArrayList<ArrayList<Integer>> adj, adj_list;
+    private final HashSet<String> citiesSet = new HashSet<>();//using set: uniqueness, no duplicates
+    private ArrayList<String> combine; //<All cities no duplicate>
+    private ArrayList<ArrayList<Integer>> adj_list;
 
     public City() {
-        generateAdjList();
+        generateAdjList(); //get all the cities that are adjacent(close together)
     }
+
+    public void generateAdjList()
+    {
+        generateCities(); //generate cities no duplicates (vertices)
+        int size = getCities().size();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(size);
+        adj_list = new ArrayList<>();
+
+        for(int i = 0; i<size;i++) {
+            adj.add(new ArrayList<>());
+            adj_list.add(new ArrayList<>());
+        }
+        for(int i =0;i<size;i++) {
+            ArrayList<String> l2 = nextCity(getCities().get(i));
+            for(int j = 0; j<l2.size();j++) {
+                if(getCities().contains(l2.get(j))) {
+                    addEdge(adj, getCities().indexOf(getCities().get(i)), getCities().indexOf(l2.get(j)));
+                }
+            }
+        }
+        for(int i=0;i<size;i++) {
+            for(int j = 0; j< adj.get(i).size(); j++) {
+                if(!adj_list.get(i).contains(adj.get(i).get(j))) {
+                    adj_list.get(i).add(adj.get(i).get(j));
+                }
+            }
+        }
+    }
+    
     public void generateCities() {
         int size = r.getSize();
         combine = new ArrayList<>();
@@ -26,6 +55,7 @@ public class City {
             }
         }
     }
+    //get neighbor cities
     public ArrayList<String> nextCity(String city) {
         ArrayList<String> result = new ArrayList<>();
         for(int i = 0;i<r.getSize();i++) {
@@ -42,45 +72,18 @@ public class City {
     public ArrayList<String> getCities() {
         return combine;
     }
+    //getter function for size of the city list
     public int getSize() {
         return combine.size();
-    }
-    public void generateAdjList()
-    {
-        generateCities(); //generate cities no duplicates
-        int size = getCities().size();
-        adj = new ArrayList<ArrayList<Integer>>(size);
-        adj_list = new ArrayList<ArrayList<Integer>>();
-
-        for(int i = 0; i<size;i++) {
-            adj.add(new ArrayList<>());
-            adj_list.add(new ArrayList<>());
-        }
-
-        for(int i =0;i<size;i++) {
-            ArrayList<String> l2 = nextCity(getCities().get(i));
-            for(int j = 0; j<l2.size();j++) {
-                if(getCities().contains(l2.get(j))) {
-                    addEdge(adj,getCities().indexOf(getCities().get(i)),getCities().indexOf(l2.get(j)));
-                }
-            }
-        }
-        
-        for(int i=0;i<size;i++) {
-            for(int j = 0;j<adj.get(i).size();j++) {
-                if(!adj_list.get(i).contains(adj.get(i).get(j))) {
-                    adj_list.get(i).add(adj.get(i).get(j));
-                }
-            }
-        }
     }
     // return the adjacency list of all vertices
     public ArrayList<ArrayList<Integer>> getAdjList() {
         return adj_list;
     }
-    // create a adjacency list
+    // create an adjacency list
     public void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v) {
         adj.get(u).add(v);
         adj.get(v).add(u);
     }
 }
+
