815a25ae5d17705285466cf4bdda234303c59105
Dion Feng <55362194+DioFeng@users.noreply.github.com>
2021-07-31 20:27:20 -0700

    Update Places.java

--- Places.java
+++ Places.java
@@ -2,53 +2,40 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class Roads {
-    private final List<Integer> distance;
-    private final List<Integer> time;
-    private final List<String> initCity, endCity;
+public class Places {
+    private final List<String> attractions;
+    private final List<String> locations;
 
-    public Roads()
-    {
-        distance = new ArrayList<>();
-        time = new ArrayList<>();
-        initCity = new ArrayList<>();
-        endCity = new ArrayList<>();
-        importData(); // initiate lists
+    public Places() {
+        attractions = new ArrayList<>();
+        locations = new ArrayList<>();
+        getData(); //store data
     }
-    // store data
-    private void importData() {
+    private void getData() {
         try {
-            File file = new File("src/roads.csv");
+            File file = new File("src/attractions.csv");
             Scanner sc = new Scanner(file);
-            //sc.nextLine();
-            while(sc.hasNextLine())
-            {
+            sc.nextLine();
+            while(sc.hasNextLine()) {
                 String line = sc.nextLine();
                 String[] result = line.split(",");
-
-                endCity.add(result[0]);
-                initCity.add(result[1]);
-                int d = Integer.parseInt(result[2]);
-                distance.add(d);
-                int t = Integer.parseInt(result[3]);
-                time.add(t);
+                attractions.add(result[0]);
+                locations.add(result[1]);
             }
         }
         catch (FileNotFoundException e)
         {
-            System.out.println("Not such file");
+            System.out.println("Not found");
         }
     }
-    //getters
-    
-    public String getEndCity(int i) { return endCity.get(i); }
-    public String getInitCity(int i) { return initCity.get(i); }
-    //cost in miles
-    public int getDistance(int i) { return distance.get(i); }
-    //cost in minutes
-    public int getMinute(int i) { return time.get(i); }
-    public int getSize() { return endCity.size(); }
-    public List<String> endCity() { return endCity; }
-    public List<String> initCity() { return initCity; }
+    //return location
+    public String getLocation(int i) { return locations.get(i); }
+    //return attraction
+    public String getAttraction(int i) { return attractions.get(i); }
+    //return a list of attraction
+    public List<String> attractions() { return attractions; }
+    //return a list of location
+    public List<String> locations() { return locations; }
+    //get data from file and store them
 
 }
