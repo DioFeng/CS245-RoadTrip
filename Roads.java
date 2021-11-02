d5ba61544eaa227489021d7e56553f0e62113164
Dion Feng <55362194+DioFeng@users.noreply.github.com>
2021-11-02 09:50:45 -0700

    Reformat

--- Roads.java
+++ Roads.java
@@ -1,28 +1,29 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Roads {
     private final List<Integer> distance;
     private final List<Integer> time;
     private final List<String> initCity, endCity;
 
-    public Roads()
-    {
+    public Roads() {
         distance = new ArrayList<>();
         time = new ArrayList<>();
         initCity = new ArrayList<>();
         endCity = new ArrayList<>();
         importData(); // initiate lists
     }
+
     // store data
     private void importData() {
         try {
-            File file = new File("roads.csv");
+            File file = new File("src/roads.csv");
             Scanner sc = new Scanner(file);
             //sc.nextLine();
-            while(sc.hasNextLine())
-            {
+            while (sc.hasNextLine()) {
                 String line = sc.nextLine();
                 String[] result = line.split(",");
 
@@ -33,22 +34,41 @@ public class Roads {
                 int t = Integer.parseInt(result[3]);
                 time.add(t);
             }
-        }
-        catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             System.out.println("Not such file");
         }
     }
     //getters
 
-    public String getEndCity(int i) { return endCity.get(i); }
-    public String getInitCity(int i) { return initCity.get(i); }
+    public String getEndCity(int i) {
+        return endCity.get(i);
+    }
+
+    public String getInitCity(int i) {
+        return initCity.get(i);
+    }
+
     //cost in miles
-    public int getDistance(int i) { return distance.get(i); }
+    public int getDistance(int i) {
+        return distance.get(i);
+    }
+
     //cost in minutes
-    public int getMinute(int i) { return time.get(i); }
-    public int getSize() { return endCity.size(); }
-    public List<String> endCity() { return endCity; }
-    public List<String> initCity() { return initCity; }
+    public int getMinute(int i) {
+        return time.get(i);
+    }
+
+    public int getSize() {
+        return endCity.size();
+    }
+
+    public List<String> endCity() {
+        return endCity;
+    }
+
+    public List<String> initCity() {
+        return initCity;
+    }
+
 
 }
